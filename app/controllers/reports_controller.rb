d428f8b5c0ba08ea322644bb49c90283b8792a0e
Hedi van Meurs <alisoun@gmail.com>
2018-08-28 17:05:06 +0100

    whoops, havent for a long time

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   before_action :find_report, only: [:show, :delete]
-  before_action :set_user, only: [:new, :index, :show]
+  before_action :set_user, only: [:new, :index, :show, :create]
 
 
   def new
@@ -13,16 +13,18 @@ class ReportsController < ApplicationController
     if @report.save
       redirect_to report_path(@report)
     else
-      render "new"
+      render "reports"
     end
   end
 
   def index
     @reports = Report.all.order(created_at: :asc)
+    @report = Report.new
   end
 
   def show
     @notes = @report.notes
+    @note = Note.new
 
     time = []
     @notes.each do |note|
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get '/pages/dashboard', to: 'pages#dashboard', as: 'dashboard'
   # get '', to: 'notes#show', as'show_note'
   resources :reports, except: [:update] do
-    resources :notes, except: [:index]
+    resources :notes, except: [:index, :destroy]
   end
+  resources :notes, only: [:destroy]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
