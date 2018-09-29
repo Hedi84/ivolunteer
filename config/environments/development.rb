99adf58b8fb4514f3d8e95c43a56114b3d03eb25
Hedi van Meurs <alisoun@gmail.com>
2018-09-29 14:52:03 +0100

    devise needs styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,22 @@
 Rails.application.configure do
+
+
+
+
+  #mailservice devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+
+  #   config.action_mailer.smtp_settings = {
+  #   address: “smtp.gmail.com”,
+  #   port: 587,
+  #   domain: ENV[“GMAIL_DOMAIN”],
+  #   authentication: “plain”,
+  #   enable_starttls_auto: true,
+  #   user_name: ENV[“GMAIL_USERNAME”],
+  #   password: ENV[“GMAIL_PASSWORD”]
+  #   }
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = true
   # Settings specified here will take precedence over those in config/application.rb.
